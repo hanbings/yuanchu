d2bfdc2f4b954a58b012d89a0dd640ea4c1359af
hanbings <hanbings@hanbings.io>
2025-04-29 00:52:14 +0800

    ✨ Feat: Impl the ffi interface for executing machine code.

--- stub/src/lib.rs
+++ stub/src/lib.rs
@@ -1,11 +1,6 @@
-use std::{ffi::c_int, io::stdin};
+use std::io::stdin;
 
 use termion::{event::Key, input::TermRead};
-
-#[unsafe(no_mangle)]
-pub extern "C" fn print_input(input: c_int) {
-    print!("{}", input);
-}
 
 #[unsafe(no_mangle)]
 pub extern "C" fn wait_input() -> u32 {
@@ -22,4 +17,47 @@ pub extern "C" fn wait_input() -> u32 {
     }
 
     0
+}
+
+/// This interface is reserved for JIT optimization.
+/// It accepts the machine code in the form of bytes passed from
+/// the calling side and places it in the memory to execute it as a function call.
+///
+/// It currently only supports x86.
+#[unsafe(no_mangle)]
+extern "C" fn run_asm(code: *const u8, size: u64) -> u64 {
+    let code = unsafe { std::slice::from_raw_parts(code, size as usize) };
+    let code = code.to_vec();
+
+    use libc::{MAP_ANON, MAP_PRIVATE, PROT_EXEC, PROT_READ, PROT_WRITE, mmap, mprotect, munmap};
+    use std::mem::transmute;
+    use std::ptr::{copy_nonoverlapping, null_mut};
+
+    unsafe {
+        let size = code.len();
+        let addr = mmap(
+            null_mut(),
+            size,
+            PROT_READ | PROT_WRITE,
+            MAP_PRIVATE | MAP_ANON,
+            -1,
+            0,
+        );
+
+        if addr == libc::MAP_FAILED {
+            panic!("mmap failed");
+        }
+
+        copy_nonoverlapping(code.as_ptr(), addr as *mut u8, size);
+        if mprotect(addr, size, PROT_EXEC) != 0 {
+            panic!("mprotect failed");
+        }
+
+        let func: extern "C" fn() -> u64 = transmute(addr);
+        let result = func();
+
+        munmap(addr, size);
+
+        result
+    }
 }
